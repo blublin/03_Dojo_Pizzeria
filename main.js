d67c8f9d361ba79f39ef6fabbd8fd2e3390420ec
Ben Lublin <lublin.ben@gmail.com>
2022-03-27 11:21:33 -0700

    begun adding bonus function

--- main.js
+++ main.js
@@ -8,10 +8,16 @@ function pizzaOven(crust, sauce, cheeses, toppings) {
     return pizza;
 }
 
+function makeMeAPizza() {
+    var crustOptions = ["Neapolitan", "New York", "Saint Louis", "Thick Crust", "Grandma", "Whole wheat"];
+    var sauceOptions = ["Marinara", "Buffalo sauce", "Pesto", "white garlic sauce", "ranch sauce", "hummus"];
+    var 
+}
+
 var notPizza = pizzaOven("deep dish", "traditional", ["mozzarella"],["pepperoni", "sausage"]);
 var goodPizza = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
 var saintLouisPizza = pizzaOven("thin crust", "marinara", ["provel"], ["pepperoni", "sausage"]);
-var specialtyPizza = pizzaOven("whole wheat", "white sauce", ["burrata", "[parmesan"], ["basil", "prosciutto"]);
+var specialtyPizza = pizzaOven("whole wheat", "white sauce", ["burrata", "parmesan"], ["basil", "prosciutto"]);
 
 console.log("I'm not sure who calls this a pizza but those wackos over in Chicago.\n", notPizza);
 console.log("Now this is a good, simple veggie pizza.\n", goodPizza);
